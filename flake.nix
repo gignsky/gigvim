73aa7975059fbb14d84af5d0e00b29b1988d4431
Maxwell Rupp <gig@gignsky.com>
2025-07-28 22:15:40 -0400

    added home-manager module

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
       # # for example:
       # inputs.obsidian-nvim.follows = "obsidian-nvim"; # <- this will use the obsidian-nvim from your inputs
     };
+    gigdot.url = "github:gignsky/dotfiles";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "gigdot/nixpkgs";
+    };
   };
 
   outputs =
@@ -36,5 +41,38 @@
           packages.default = nvimConfig.neovim;
           formatter = pkgs.alejandra;
         };
+
+      flake = {
+        homeManagerModules.default = { config, lib, pkgs, ... }: {
+          options.programs.gigvim = {
+            enable = lib.mkEnableOption "GigVim Neovim configuration";
+            
+            package = lib.mkOption {
+              type = lib.types.package;
+              description = "The Neovim package to use";
+              default = let
+                configModule = import ./nvf-config.nix;
+                nvimConfig = nvf.lib.neovimConfiguration {
+                  modules = [ configModule ];
+                  inherit pkgs;
+                };
+              in nvimConfig.neovim;
+            };
+          };
+
+          config = lib.mkIf config.programs.gigvim.enable {
+            home.packages = [ config.programs.gigvim.package ];
+            
+            # Optional: Set as default editor
+            home.sessionVariables = {
+              EDITOR = lib.mkDefault "${config.programs.gigvim.package}/bin/nvim";
+              VISUAL = lib.mkDefault "${config.programs.gigvim.package}/bin/nvim";
+            };
+          };
+        };
+
+        # Alias for convenience
+        homeManagerModules.gigvim = inputs.self.homeManagerModules.default;
+      };
     };
 }
