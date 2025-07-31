041d5dfbece74e7d4ffdc3a66a275b8711b2353a
Maxwell Rupp <gig@gignsky.com>
2025-07-31 03:37:29 -0400

    configured some nix lsp stuff

--- languages/nix.nix
+++ languages/nix.nix
@@ -1,9 +1,15 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   config.vim.languages.nix = {
     enable = true;
     format = {
       type = "alejandra";
       package = pkgs.alejandra;
     };
+    lsp = {
+      enable = true;
+      package = pkgs.nil;
+      server = "nil";
+    };
+    treesitter.enable = true;
   };
 }
