8bb7b2e3a140c7e61ae5c0e6c138a021a6719bf5
Maxwell Rupp <gig@gignsky.com>
2025-08-05 21:22:43 -0400

    disabled dap and set rust-analyzer package

--- lang/rust.nix
+++ lang/rust.nix
@@ -5,13 +5,23 @@
       enable = true;
       codeActions = true;
     };
-    dap.enable = true;
+    # dap.enable = false;
     format = {
       enable = true;
       type = "rustfmt";
     };
     lsp = {
       enable = true;
+      package = pkgs.rust-analyzer;
+      opts = ''
+        ['rust-analyzer'] = {
+            cargo = {allFeature = true},
+            checkOnSave = true,
+            procMacro = {
+              enable = true,
+            },
+          },
+      '';
     };
     treesitter.enable = true;
   };
