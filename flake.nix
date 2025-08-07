334f76e16f5f1c3a233fad9028542365363e75aa
Maxwell Rupp <gig@gignsky.com>
2025-08-07 03:57:49 -0400

    Squash merge add-additional-outputs into main

--- flake.nix
+++ flake.nix
@@ -13,12 +13,13 @@
     home-manager.follows = "gigdot/nixpkgs";
   };
 
-  outputs = {
-    flake-parts,
-    nvf,
-    ...
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    {
+      flake-parts,
+      nvf,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
         "x86_64-darwin"
@@ -26,56 +27,63 @@
         "aarch64-darwin"
       ];
 
-      perSystem = {pkgs, ...}: let
-        minimalConfigModule = import ./minimal.nix;
-        fullConfigModule = import ./full.nix;
-        fullNvimConfig = nvf.lib.neovimConfiguration {
-          modules = [fullConfigModule];
-          inherit pkgs;
+      perSystem =
+        { pkgs, ... }:
+        let
+          minimalConfigModule = import ./minimal.nix;
+          fullConfigModule = import ./full.nix;
+          fullNvimConfig = nvf.lib.neovimConfiguration {
+            modules = [ fullConfigModule ];
+            inherit pkgs;
+          };
+          minimalNvimConfig = nvf.lib.neovimConfiguration {
+            modules = [ minimalConfigModule ];
+            inherit pkgs;
+          };
+        in
+        {
+          packages.minimal = minimalNvimConfig.neovim;
+          packages.mini = minimalNvimConfig.neovim;
+          packages.default = fullNvimConfig.neovim;
+          packages.full = fullNvimConfig.neovim;
+          packages.gigvim = fullNvimConfig.neovim;
+          formatter = pkgs.nixfmt;
+          devShells.default = pkgs.mkShell {
+            packages = [
+              fullNvimConfig.neovim
+            ];
+          };
         };
-        minimalNvimConfig = nvf.lib.neovimConfiguration {
-          modules = [minimalConfigModule];
-          inherit pkgs;
-        };
-      in {
-        packages.minimal = minimalNvimConfig.neovim;
-        packages.default = fullNvimConfig.neovim;
-        packages.full = fullNvimConfig.neovim;
-        formatter = pkgs.nixfmt;
-        devShells.default = pkgs.mkShell {
-          packages = [
-            fullNvimConfig.neovim
-          ];
-        };
-      };
 
       flake = {
-        homeManagerModules.default = {
-          config,
-          lib,
-          pkgs,
-          ...
-        }: {
-          options.programs.gigvim = {
-            enable = lib.mkEnableOption "GigVim Neovim configuration";
+        homeManagerModules.default =
+          {
+            config,
+            lib,
+            pkgs,
+            ...
+          }:
+          {
+            options.programs.gigvim = {
+              enable = lib.mkEnableOption "GigVim Neovim configuration";
 
-            package = lib.mkOption {
-              type = lib.types.package;
-              description = "The Neovim package to use";
-              default = inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.full;
+              package = lib.mkOption {
+                type = lib.types.package;
+                description = "The Neovim package to use";
+                default = inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.full;
+              };
+            };
+
+            config = lib.mkIf config.programs.gigvim.enable {
+              home.packages = [ config.programs.gigvim.package ];
+
+              # Optional: Set as default editor
+              home.sessionVariables = {
+                EDITOR = lib.mkDefault "${config.programs.gigvim.package}/bin/nvim";
+                VISUAL = lib.mkDefault "${config.programs.gigvim.package}/bin/nvim";
+              };
             };
           };
-
-          config = lib.mkIf config.programs.gigvim.enable {
-            home.packages = [config.programs.gigvim.package];
-
-            # Optional: Set as default editor
-            home.sessionVariables = {
-              EDITOR = lib.mkDefault "${config.programs.gigvim.package}/bin/nvim";
-              VISUAL = lib.mkDefault "${config.programs.gigvim.package}/bin/nvim";
-            };
-          };
-        };
 
         # Alias for convenience
         homeManagerModules.gigvim = inputs.self.homeManagerModules.default;
