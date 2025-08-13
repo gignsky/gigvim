66bcb0c7705878d806981252105dad538f3576ad
Maxwell Rupp <gig@gignsky.com>
2025-08-13 01:52:38 -0400

    moved git-dev keybinds

--- binds/default.nix
+++ binds/default.nix
@@ -1,53 +1,14 @@
 {
+  imports = [
+    ./git-dev-nvim.nix
+  ];
+
   config.vim.keymaps = [
     {
       key = "jj";
       mode = "i";
       silent = true;
       action = "<Esc>";
-    }
-    # git-dev.nvim keybinds
-    {
-      key = "<leader>go";
-      mode = "n";
-      silent = true;
-      action = ":GitDevOpen ";
-      description = "Open remote repository";
-    }
-    {
-      key = "<leader>gr";
-      mode = "n";
-      silent = true;
-      action = ":GitDevRecents<CR>";
-      description = "Browse recent repositories";
-    }
-    {
-      key = "<leader>gc";
-      mode = "n";
-      silent = true;
-      action = ":GitDevClean<CR>";
-      description = "Clean current repository";
-    }
-    {
-      key = "<leader>gC";
-      mode = "n";
-      silent = true;
-      action = ":GitDevCleanAll<CR>";
-      description = "Clean all cached repositories";
-    }
-    {
-      key = "<leader>gb";
-      mode = "n";
-      silent = true;
-      action = ":GitDevCloseBuffers<CR>";
-      description = "Close all buffers for current repository";
-    }
-    {
-      key = "<leader>gu";
-      mode = "n";
-      silent = true;
-      action = ":GitDevToggleUI<CR>";
-      description = "Toggle git-dev output window";
     }
   ];
 }
