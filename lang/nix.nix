4675bfbb508a37e83150adef77612b09479dbcd8
Maxwell Rupp <maxwell@giglab.dev>
2026-04-14 22:42:38 -0400

    moved lsp config to get things building again

--- lang/nix.nix
+++ lang/nix.nix
@@ -7,32 +7,29 @@
       type = "nixfmt";
       # package = pkgs.nixfmt-rfc-style;
     };
-    lsp = {
-      enable = true;
-      package = pkgs.nil;
-      server = "nil";
-      options = {
-        "nil" = {
-          "formatting" = {
-            "command" = [ "${pkgs.nixfmt-rfc-style}/bin/nixfmt" ];
-          };
-          "nix" = {
-            "flake" = {
-              "autoArchive" = true;
-              "autoEvalInputs" = true;
-            };
-          };
-        };
-      };
-    };
     treesitter.enable = true;
   };
 
   config.vim.lsp.servers.nil = {
+    enable = true;
+    package = pkgs.nil;
+    server = "nil";
+    options = {
+      "nil" = {
+        "formatting" = {
+          "command" = [ "${pkgs.nixfmt-rfc-style}/bin/nixfmt" ];
+        };
+        "nix" = {
+          "flake" = {
+            "autoArchive" = true;
+            "autoEvalInputs" = true;
+          };
+        };
+      };
+    };
     settings.nixos-options = {
       expr = "builtins.getFlake(toString ./.);";
     };
-    enable = true;
   };
 
   config.vim.snippets.luasnip = {
